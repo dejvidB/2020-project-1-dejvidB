a403979719132d6c6b2a2b36640ba796428ed406
Dejvid Bushi <dejvid.bushi@gmail.com>
2020-03-12 16:15:42 +0200

    make function pq_sort_vector

--- modules/pq_sort.c
+++ modules/pq_sort.c
@@ -9,23 +9,38 @@
 #include "pq_sort.h"
 #include "ADTPriorityQueue.h"
 
+int compare_ints(Pointer a, Pointer b)
+{
+	return *(int *)a - *(int *)b;
+}
 
-void pq_sort_vector(Vector vec, CompareFunc compare) {
+int *create_int(int value)
+{
+	int *pointer = malloc(sizeof(int)); // δέσμευση μνήμης
+	*pointer = value;					// αντιγραφή του value στον νέο ακέραιο
+	return pointer;
+}
+
+void pq_sort_vector(Vector vec, CompareFunc compare)
+{
 	// Προσοχή! Μέσα στη συνάρτηση αυτή θα χρειαστεί να αντικαταστήσουμε τα περιεχόμενα του vector. Δε
 	// θέλουμε όμως όσο το κάνουμε αυτό το vector να καλέσει τη destroy! Οπότε αλλάζουμε προσωρινά τη
 	// συνάρτηση destroy σε NULL (αποθηκεύοντας την παλιά τιμή).
 	DestroyFunc old_destroy = vector_set_destroy_value(vec, NULL);
 
-
 	// κυρίως λειτουργία της συνάρτησης
-	// ...
-
+	PriorityQueue pq = pqueue_create(compare_ints, free, vec);
+	for (int i = 0; i < vector_size(vec); i++)
+	{
+		vector_set_at(vec, i, create_int(*((int *)pqueue_max(pq))));
+		pqueue_remove_max(pq);
+	}
 	// επαναφορά της destroy
 	vector_set_destroy_value(vec, old_destroy);
 }
 
-void pq_sort_list(List list, CompareFunc compare) {
+void pq_sort_list(List list, CompareFunc compare)
+{
 
 	// ...
-
 }
