28d719f3b52513ef3dc08a20ca8b1bebcc70fbd5
Dejvid Bushi <dejvid.bushi@gmail.com>
2020-03-16 19:56:07 +0200

    life_gif.c first trial

--- programs/life_gif/life_gif.c
+++ programs/life_gif/life_gif.c
@@ -1,13 +1,38 @@
-/////////////////////////////////////////////////////////////////////////
-//
-// Παράδειγμα δημιουργίας ενός GIF χρησιμοποιώντας τη βιβλιοθήκη libbmp
-//
-///////////////////////////////////////////////////////////////////////// 
-
 #include "bmp.h"
 #include "gif.h"
+#include "ADTVector.h"
+#include <string.h>
+#include "life.h"
+
+typedef char* String;
 
 int main(int argc, char *argv[]) {
+    if(argc < 11)
+        return 1;
+    
+    int frames;
+    if((frames = atoi(argv[6])) < 1) //frames >= 1
+        return 1;
+
+    float zoom;
+    if((zoom = atof(argv[7]) < 0))  //zoom
+        return 1;
+
+    int speed;
+    if((speed = atoi(argv[8])) < 1)
+        return 1;
+
+    int delay;
+    if((delay = atoi(argv[9])) < 0)
+        return 1;
+
+    String state = strdup(argv[1]);
+    int top = atoi(argv[2]), left = atoi(argv[3]), bottom = atoi(argv[4]), right = atoi(argv[5]);
+    
+    String target_gif = strdup(argv[10]);
+
+    LifeState state = life_create_from_rle(state);
+
 	// Μεγέθη
 	int size = 128;
 	int cell_size = 5;
@@ -23,22 +48,48 @@ int main(int argc, char *argv[]) {
 	// γωνία προς την κάτω-αριστερά. Το cell μετακινείται ένα pixel τη φορά, οπότε το animation
 	// θα έχει τόσα frames όσα το μέθεθος της εικόνας.
 
-	for(int i = 0; i < size; i++) {
-		// Σε κάθε frame, πρώτα μαυρίζουμε ολόκληρο το bitmap
-		bm_set_color(bitmap, bm_atoi("black"));
-		bm_clear(bitmap);
+    //Απεικόνιση αρχικής κατάστασης
+    //πρώτα μαυρίζουμε ολόκληρο το bitmap
+	bm_set_color(bitmap, bm_atoi("black"));
+	bm_clear(bitmap);
+    for(MapNode map_node = map_first(state); map_node != MAP_EOF; map_node = map_next(state, map_node)){
+        Set line = map_node_value(state, map_node);
+        for(SetNode node = set_first(line); node != SET_EOF; node = set_next(line, node)){
+            LifeCell cell;
+            cell.x = ((LifeCell*)set_node_value(line, node))->x;
+            cell.y = ((LifeCell*)set_node_value(line, node))->y;
+            // Και μετά ζωγραφίζουμε ένα άσπρο τετράγωνο με αρχή το
+            // σημείο (i,i) και τέλος το (i+cell_size, i+cell_size)
+            bm_set_color(bitmap, bm_atoi("white"));
+            bm_fillrect(bitmap, cell.x + size/2, cell.y + size/2, cell.x + size/2 + cell_size, cell.y + size/2 + cell_size);
+            // Τέλος προσθέτουμε το bitmap σαν frame στο GIF (τα περιεχόμενα αντιγράφονται)
+            gif_add_frame(gif, bitmap);
+        }
+    }
 
-		// Και μετά ζωγραφίζουμε ένα άσπρο τετράγωνο με αρχή το
-		// σημείο (i,i) και τέλος το (i+cell_size, i+cell_size)
-		bm_set_color(bitmap, bm_atoi("white"));
-		bm_fillrect(bitmap, i, i, i+cell_size, i+cell_size);
-   
-		// Τέλος προσθέτουμε το bitmap σαν frame στο GIF (τα περιεχόμενα αντιγράφονται)
-		gif_add_frame(gif, bitmap);
-	}
+    for(int i = 1; i < frames; i++){
+        //Evlove speed φορές
+        for(int j = 0; j < speed; j++){
+            state = life_evolve(state);
+        }
+        for(MapNode map_node = map_first(state); map_node != MAP_EOF; map_node = map_next(state, map_node)){
+            Set line = map_node_value(state, map_node);
+            for(SetNode node = set_first(line); node != SET_EOF; node = set_next(line, node)){
+                LifeCell cell;
+                cell.x = ((LifeCell*)set_node_value(line, node))->x;
+                cell.y = ((LifeCell*)set_node_value(line, node))->y;
+                // Και μετά ζωγραφίζουμε ένα άσπρο τετράγωνο με αρχή το
+                // σημείο (i,i) και τέλος το (i+cell_size, i+cell_size)
+                bm_set_color(bitmap, bm_atoi("white"));
+                bm_fillrect(bitmap, cell.x + size/2, cell.y + size/2, cell.x + size/2 + cell_size, cell.y + size/2 + cell_size);
+                // Τέλος προσθέτουμε το bitmap σαν frame στο GIF (τα περιεχόμενα αντιγράφονται)
+                gif_add_frame(gif, bitmap);
+            }
+        }
+    }
 
 	// Αποθήκευση σε αρχείο
-	gif_save(gif, "example.gif");
+	gif_save(gif, target_gif);
 
 	// Αποδέσμευση μνήμης
 	bm_free(bitmap);
